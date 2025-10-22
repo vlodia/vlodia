f330cb0e931f4c5a54775245c7496df306abce11
txkachi <yahyabey2401@icloud.com>
2025-10-22 23:01:01 +0300

    Delete dist directory

--- dist/cli/index.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * CLI Entry Point
- * Main CLI application for Nythera ORM
- * Provides command-line interface for development workflow
- */
-export { CLICommands } from './commands';
-export * from './commands';
-//# sourceMappingURL=index.d.ts.map
--- dist/types/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-/**
- * Core type definitions for Nythera ORM
- * Defines the fundamental interfaces and types used throughout the system
- */
-Object.defineProperty(exports, "__esModule", { value: true });
-//# sourceMappingURL=index.js.map
